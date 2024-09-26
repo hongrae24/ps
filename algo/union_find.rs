bd66c0015f9055c3da69aa3043e2d97c5052e277
hongrae <hongrae24@gmail.com>
2024-09-26 20:43:45 +0900

    template of union find

---
+++ algo/union_find.rs
@@ -0,0 +1,17 @@
+fn find(tree: &mut Vec<usize>, u: usize) -> usize {
+    if tree[u] == u {
+        return u;
+    } else {
+        tree[u] = find(tree, tree[u]);
+        return tree[u];
+    }
+}
+
+fn merge(tree: &mut Vec<usize>, u: usize, v: usize) {
+    let u = find(tree, u);
+    let v = find(tree, v);
+    if u == v {
+        return;
+    }
+    tree[u] = v;
+}
